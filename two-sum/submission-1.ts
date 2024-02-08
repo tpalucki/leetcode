2d9437e6326d5e36d372427d107f16e7c6200a2e
Tomasz Pałucki <tomasz.palucki@payzilch.com>
2024-02-08 14:37:17 +0100

    Added solution for  problem

---
+++ two-sum/submission-1.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const numberMap = new Map();
+
+    for (let i = 0; i < nums.length; i++ ) {
+        const difference = target - nums[i];
+
+        if (numberMap.has(difference)) {
+            return [numberMap.get(difference), i]
+        }
+        numberMap.set(nums[i], i);
+    }
+};
